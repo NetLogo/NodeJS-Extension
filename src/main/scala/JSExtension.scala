667c68130ddbb390a8e132265a6b1729168f378c
James Hovet <jrhovet@gmail.com>
2021-05-16 22:40:23 -0500

    first working version

--- src/main/scala/JSExtension.scala
+++ src/main/scala/JSExtension.scala
@@ -1,23 +1,102 @@
 package org.nlogo.extensions.js
+
+import com.fasterxml.jackson.core.JsonParser
+import org.json4s.jackson.JsonMethods.mapper
+import org.me.Subprocess
+import org.nlogo.api
 import org.nlogo.core.Syntax
 import org.nlogo.core.Syntax._
-import org.nlogo.api.{Argument, Context, DefaultClassManager, ExtensionException, LogoException, PrimitiveManager, Reporter}
+import org.nlogo.api.{Argument, Context, DefaultClassManager, ExtensionException, ExtensionManager, LogoException, PrimitiveManager, Reporter}
 import org.nlogo.api.ScalaConversions._
 
+import java.io.File
+
+object JSExtension {
+  private var _nodeProcess: Option[Subprocess] = None
+
+  val extDirectory: File = new File(
+    getClass.getClassLoader.asInstanceOf[java.net.URLClassLoader].getURLs()(0).toURI.getPath
+  ).getParentFile
+
+  def nodeProcess: Subprocess =
+    _nodeProcess.getOrElse(throw new ExtensionException((
+      "Node.JS Process has not been started. Please run js:setup first before any other js extension primitive"
+    )))
+
+  def nodeProcess_=(proc: Subprocess): Unit = {
+    _nodeProcess.foreach(_.close())
+    _nodeProcess = Some(proc)
+  }
+
+  def killNode(): Unit = {
+    _nodeProcess.foreach(_.close())
+    _nodeProcess = None
+  }
+
+}
+
 class JSExtension extends DefaultClassManager {
-  def load(manager: PrimitiveManager) {
-    manager.addPrimitive("hello", new HelloString)
+  def load(manager: PrimitiveManager): Unit = {
+    manager.addPrimitive("setup", SetupNode)
+    manager.addPrimitive("run", Run)
+    manager.addPrimitive("runresult", RunResult)
+    manager.addPrimitive("set", Set)
+  }
+
+  override def runOnce(em: ExtensionManager): Unit = {
+    super.runOnce(em)
+    mapper.configure(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS, true)
+  }
+
+  override def unload(em: ExtensionManager): Unit = {
+    super.unload(em);
+    JSExtension.killNode()
   }
 }
 
-class HelloString extends Reporter {
-  override def getSyntax = reporterSyntax(right = List(StringType), ret = StringType)
-  def report(args: Array[Argument], context: Context): AnyRef = {
-    val name = try args(0).getString
-    catch {
-      case e: LogoException =>
-        throw new ExtensionException(e.getMessage)
+object SetupNode extends api.Command {
+  override def getSyntax: Syntax = Syntax.commandSyntax(right = List(Syntax.ListType))
+
+  override def perform(args: Array[Argument], context: Context): Unit = {
+
+    // TODO: Because of the way the global eval works, require() doesn't work. Without changing the subprocess api to
+    // add another type of message or add custom messages, I think the best workaround is to allow the user to add
+    // in a few lines of code as a "warmup" step and we just prepend them to jsext.js before runtime.
+    // That said, this might be a message that I need to add more/arbitrarily many message types.
+
+    val jsScript: String = new File(JSExtension.extDirectory, "jsext.js").toString
+    try {
+      JSExtension.nodeProcess = Subprocess.start(context.workspace, Seq("node"), Seq(jsScript), "js", "Node.js Javascript")
+    } catch {
+      case e: Exception => {
+        println(e)
+        throw new ExtensionException("didn't want to start")
+      }
     }
-    "hello, " + name
   }
+}
+
+object Run extends api.Command {
+  override def getSyntax: Syntax = Syntax.commandSyntax(
+    right = List(Syntax.StringType | Syntax.RepeatableType)
+  )
+
+  override def perform(args: Array[Argument], context: Context): Unit =
+    JSExtension.nodeProcess.exec(args.map(_.getString).mkString("\n")).get.get
+}
+
+object RunResult extends api.Reporter {
+  override def getSyntax: Syntax = Syntax.reporterSyntax(
+    right = List(Syntax.StringType | Syntax.RepeatableType),
+    ret = Syntax.WildcardType
+  )
+
+  override def report(args: Array[Argument], context: Context): AnyRef =
+    JSExtension.nodeProcess.eval(args.map(_.getString).mkString("\n")).get.get
+}
+
+object Set extends api.Command {
+  override def getSyntax: Syntax = Syntax.commandSyntax(right = List(Syntax.StringType, Syntax.ReadableType))
+  override def perform(args: Array[Argument], context: Context): Unit =
+    JSExtension.nodeProcess.assign(args(0).getString, args(1).get).get.get
 }
